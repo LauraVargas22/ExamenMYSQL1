207b2708c7625040bc84bbf98078b80a74a1d53c
LauraVargas22 <181015028+LauraVargas22@users.noreply.github.com>
2025-03-12 09:56:24 -0500

    Update insert.sql

---
+++ storage/mysql/insert.sql
@@ -0,0 +1,5 @@
+INSERT INTO genre(description) values('Drama'), ('Comedia'), ('Ficcion'), ('Aventura');
+
+INSERT INTO writer(name, birthdate) values('Gabriel Garcia', '1970-03-10'), ('Jorge Isacc', '1976-03-23'), ('Piedad Bonnet', '1945-04-10');
+
+INSERT INTO members(name, n_document, birthdate) values('Laura', 1053443687, '2006-09-22'), ('Valentina', 80030237, '1985-05-01');
